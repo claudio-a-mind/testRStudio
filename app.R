014be7fc8dbf717becd3bacecca44325b7758246
claudio-a-mind <claudio.a@somosmind.com>
2021-07-13 18:28:54 -0400

    Commit

--- app.R
+++ app.R
@@ -1,6 +1,32 @@
 library(shiny)
 library(shinydashboard)
 library(shinydashboardPlus)
+library(shinyjs)
+library(DT)
+
+
+## Funciones dinámicas ----------------------------
+
+Icon_Account = function(i) {
+  if(with(TablaAux, Suma[Account == UTMClientes[i]] > 0)){
+    tags$i(class = "fa fa-exclamation-triangle", style = "color: red")
+  } else {
+    tags$i(class = "fa fa-exclamation-triangle", style = "color: green")
+  }
+}
+
+Vbox_UTM = function(i){
+  error = ifelse(with(TablaAux, Suma[Account == UTMClientes[i]] > 0), 1, 0)
+  n_error = with(TablaAux, Suma[Account == UTMClientes[i]])   
+  
+  valueBox(
+    subtitle = "Indicador error UTM" ,
+    value = ifelse(error == 1 ,paste("Existen ",n_error," errores."),"No Error"),
+    icon = icon("exclamation-circle"),
+    color = ifelse(error == 1 ,"red","green")
+  )
+}
+
 
 ## Personalización --------------------------------
 
@@ -33,9 +59,13 @@ shinyApp(
     
     ## Barra lateral
     sidebar = dashboardSidebar(
-      menuItem("Inicio", tabName = "inicio", icon = icon("home")),
-      menuItem("Alerta 1", tabName = "alerta1", icon = icon("alert")),
-      minified = TRUE, collapsed = FALSE),
+      sidebarMenu(id = "tabs", minified = T, collapsed = F,
+        menuItem("Inicio", tabName = "inicio", icon = icon("home")),
+        menuItem(UTMClientes[1], tabName = "usuario1", icon = icon("alert")),
+        menuItem(UTMClientes[2], tabName = "usuario2", icon = icon("alert")),
+        menuItem(UTMClientes[3], tabName = "usuario3", icon = icon("alert"))
+        )
+      ),
     
     ## Cuerpo de la app
     body = dashboardBody(
@@ -46,42 +76,46 @@ shinyApp(
       # Definir contenidos de la app
       tabItems(
         ## ITEM: Inicio
-        tabItem("inicio", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor est in aliquet egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras tristique, nisl vel fringilla efficitur, nulla nibh malesuada risus, eget congue magna orci vel ligula. Aliquam sit amet augue tortor. Integer egestas placerat tortor sit amet aliquet. Quisque luctus finibus orci vitae luctus. In egestas nec turpis nec consequat. Morbi orci augue, pulvinar dignissim sapien a, placerat bibendum est. Curabitur tristique, risus non congue mattis, neque enim pharetra enim, quis sagittis odio turpis non nisi. Morbi finibus purus magna, non consectetur turpis convallis sit amet. Praesent ultricies ipsum vel eros imperdiet finibus. Mauris et volutpat nunc, ut pretium lorem. Nunc venenatis interdum nulla, sit amet suscipit turpis congue sollicitudin.
-
-Integer vitae volutpat quam, id eleifend velit. Nulla lectus leo, congue eget molestie id, tempor et diam. Donec pretium nunc sit amet laoreet facilisis. Etiam a nulla rhoncus, vestibulum massa non, ultricies nunc. Mauris tellus nunc, finibus eget est a, porta hendrerit tortor. Maecenas sapien dui, accumsan a congue accumsan, scelerisque vitae justo. Aliquam imperdiet in felis et egestas.
-
-Ut sodales porttitor lacus, eget cursus magna eleifend ac. Pellentesque quis risus molestie, interdum dolor vitae, blandit sapien. Integer massa libero, accumsan sed ornare quis, elementum ut sem. Aenean congue orci eget volutpat finibus. Nam eget tincidunt mi. In molestie nunc eu egestas interdum. Vestibulum aliquet posuere ligula, nec pellentesque elit euismod non. Aliquam tincidunt ligula ac condimentum tempus. Nulla vestibulum dictum odio, ut fermentum nulla semper eleifend. Sed vel molestie nunc, vel fringilla sapien. In nec suscipit sapien, ut commodo urna. Phasellus scelerisque condimentum quam, condimentum imperdiet purus."),
-        
-        ## ITEM: Alerta 1
-        tabItem("alerta1",
+        tabItem("inicio",
                 fluidRow(
-                  # selectInput(inputId = "region", label = "Seleccione Región", choices = Regiones),
-                  column(6,selectInput(inputId = "cliente", label = "Seleccione Cliente", choices = UTMClientes)),
-                  column(6,selectInput(inputId = "producto", label = "Seleccione Producto", choices = NULL))
+                  box(title = span(Icon_Account(1), UTMClientes[1]),
+                      width=12, collapsible = T,
+                      actionButton("id",paste("Enlace a página de ",UTMClientes[1])),
+                      br(),
+                      valueBoxOutput("vbox1"))),
+                fluidRow(
+                  box(title = span(Icon_Account(2), UTMClientes[2]),
+                      width=12, collapsible = T, 
+                      actionButton("id2",paste("Enlace a página de ",UTMClientes[2])),
+                      br(),
+                      valueBoxOutput("vbox2"))),
+                fluidRow(
+                  box(title = span(Icon_Account(3), UTMClientes[3]),
+                      width=12, collapsible = T,
+                      actionButton("id3",paste("Enlace a página de ",UTMClientes[3])),
+                      br(),
+                      valueBoxOutput("vbox3")))
                 ),
-                # # fluidRow(
-                # #     infoBox(title = "Alerta 1", 
-                # #             subtitle = "Indicador de alerta", 
-                # #             value = 10, 
-                # #             icon = icon("exclamation-circle"),
-                # #             color="red"),
-                # #     infoBox(title = "Alerta 2", 
-                # #             subtitle = "Indicador de alerta", 
-                # #             value = 10, 
-                # #             icon = icon("exclamation-circle"),
-                # #             color="yellow"),
-                # #     infoBox(title = "Alerta 3", 
-                # #             subtitle = "Indicador de alerta", 
-                # #             value = 10, 
-                # #             icon = icon("exclamation-circle"),
-                # #             color="green")
-                # # ),
+        ## ITEM: Usuario 1 
+        tabItem("usuario1", 
                 fluidRow(
-                  # box(width = 2, actionButton("count", "Count")),
-                  # infoBoxOutput("ibox"),
-                  valueBoxOutput("vbox")
+                  actionButton("home1",paste("Volver al inicio")),
+                  box(width = 12, DTOutput('tbl1'))
+                  )
+        ),
+        ## ITEM: Usuario 2 
+        tabItem("usuario2", 
+                fluidRow(
+                  actionButton("home2",paste("Volver al inicio")),
+                  box(width = 12, DTOutput('tbl2'))
                 )
-                
+        ),
+        ## ITEM: Usuario 3 
+        tabItem("usuario3", 
+                fluidRow(
+                  actionButton("home3",paste("Volver al inicio")),
+                  box(width = 12, DTOutput('tbl3'))
+                )
         )
       )
       
@@ -99,27 +133,37 @@ Ut sodales porttitor lacus, eget cursus magna eleifend ac. Pellentesque quis ris
                         choices = producto_list)
     })
     
-    # # output$ibox <- renderInfoBox({
-    # #     infoBox(
-    # #         "Title",
-    # #         input$count,
-    # #         icon = icon("credit-card")
-    # #     )
-    # # })
     
+
+# Botones entre páginas ---------------------------------------------------
     
-    output$vbox <- renderValueBox({
-      
-      error = ifelse(with(TablaAux, Suma[Account == input$cliente] > 0), 1, 0)
-      n_error = with(TablaAux, Suma[Account == input$cliente])   
-      
-      valueBox(
-        subtitle = "Indicador error UTM" ,
-        value = ifelse(error == 1 ,paste("Existen ",n_error," errores."),"No Error"),
-        icon = icon("exclamation-circle"),
-        color = ifelse(error == 1 ,"red","green")
-      )
+    ## Ir a cada usuario 
+    observeEvent(input$id, {
+      updateTabsetPanel(session, "tabs","usuario1")
     })
+    observeEvent(input$id2, {
+      updateTabsetPanel(session, "tabs","usuario2")
+    })
+    observeEvent(input$id3, {
+      updateTabsetPanel(session, "tabs","usuario3")
+    })
+    
+    ## Volver al home
+    
+    observeEvent( (input$home1 | input$home2 | input$home3) , {
+      updateTabsetPanel(session, "tabs","inicio")
+    })
+    
+
+    ## Creación VBOX - Inicio UTM -----------------
+    output$vbox1 <- renderValueBox({ Vbox_UTM(1) })
+    output$vbox2 <- renderValueBox({ Vbox_UTM(2) })
+    output$vbox3 <- renderValueBox({ Vbox_UTM(3) })
+    
+    ## Creación DT - UTM dado Cliente
+    output$tbl1 = renderDT(UTM_Account(1))
+    output$tbl2 = renderDT( UTM_Account(2))
+    output$tbl3 = renderDT(UTM_Account(3))
   }
   
   
--- script.R
+++ script.R
@@ -1,4 +1,3 @@
-
 library(googledrive)
 library(tidyverse)
 library(readxl)
@@ -9,13 +8,14 @@ options(gargle_quiet = FALSE) # Configutación con errores
 drive_auth( email = "claudio.a@somosmind.com",
             token = "4/1AX4XfWg3sr2b6eRCJBp25Sdq2rZiJt0lfQ048iIbZLf690TB6EvY0Z3eOLI")
 
-drive_download(as_id("1XrcFhzAfzf9K4Z2MJyd-BLAqZGbyzMcPRSNtOIXVaO4") ,overwrite = TRUE, path = "FB_ads_all_account_utm_status.xlsx")
+drive_download(as_id("1XrcFhzAfzf9K4Z2MJyd-BLAqZGbyzMcPRSNtOIXVaO4"),
+               overwrite = TRUE, path = "FB_ads_all_account_utm_status.xlsx")
 df01= read_xlsx("FB_ads_all_account_utm_status.xlsx")
 
 # Cambio de formato en nombre de columna ----------------------------------
 cambiar_nombre_variables <- function(df){
   variables=colnames(df)
-  nuevas_variables=str_replace_all(str_squish((str_replace_all(variables , regex("\\W+"), " ")))," ","_")
+  nuevas_variables = str_replace_all(str_squish((str_replace_all(variables , regex("\\W+"), " ")))," ","_")
   nuevas_variables=stringi::stri_trans_general(nuevas_variables,"Latin-ASCII")
   colnames(df)=nuevas_variables
   return(df)
@@ -23,29 +23,45 @@ cambiar_nombre_variables <- function(df){
 
 
 df_3 = cambiar_nombre_variables(df01)
-names(df_3)
+
+df_31= df_3 %>% 
+  select(Account, Ad_name , Date, Link_asset_website_URL) %>% 
+  group_by(Ad_name) %>% 
+  mutate(aux_alerta_utm= grepl(pattern = "utm_", x = Link_asset_website_URL,ignore.case = TRUE), 
+         alerta_utm = if_else(aux_alerta_utm==TRUE,0,1))
 
 
-df_31= df_3 %>% select(Account, Ad_name , Date, Link_asset_website_URL) %>% group_by(Ad_name) %>% 
-  mutate(aux_alerta_utm= grepl(pattern = "utm_", x = Link_asset_website_URL , ignore.case = TRUE),
-         alerta_utm= if_else(aux_alerta_utm==TRUE,0,1))
-
-dim(df_31) 
 df_31$Date_chr = as.character(df_31$Date)
 
-head(df_31$Date_chr)
+DF_AUX = df_31 %>% filter(Date_chr == "2021-07-13")
 
-DF_AUX = df_31 %>% filter(Date_chr == "2021-07-07")
+## Creación Vector: Nombres de clientes
 UTMClientes = sort(unique(DF_AUX$Account))
 
-names(DF_AUX)
 
 TablaAux = DF_AUX %>% 
   group_by(Account) %>%
   summarise(Suma = sum(alerta_utm))
 
-TablaAux %>% filter(Account == "Banco BICE") 
 
-with(TablaAux, Suma[Account == "Banco BICE"] > 0)
+# Funciones Dashboard -----------------------------------------------------
 
-## Coment
+Icon_Account = function(i) {
+if(with(TablaAux, Suma[Account == UTMClientes[i]] > 0)){
+  tags$i(class = "fa fa-exclamation-triangle", style = "color: red")
+} else {
+  tags$i(class = "fa fa-exclamation-triangle", style = "color: green")
+}
+}
+
+UTM_Account = function(i){
+DF_AUX %>% 
+  filter(Account == UTMClientes[i]) %>% 
+  group_by(Ad_name) %>% 
+  summarise("Número Errores UTM" = sum(alerta_utm)) %>% 
+  rename(Anuncio = Ad_name) 
+}
+
+UTM_Account(10)
+
+
